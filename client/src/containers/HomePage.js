8beb2da1f85288be993f856348c5301701f71b25
sarahgraham1982 <79756187+sarahgraham1982@users.noreply.github.com>
2021-08-06 14:11:56 +0100

    nav changes and welcome image

--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const HomePage = () => (
   <div>
-    <h4>Home!</h4>
+    <h4>Home</h4>
+  </div>
+
+  <div className="welcome-image">
+    <img src="/welcome.png" alt="welcome" width="500" height="500" />
   </div>
 );
 
