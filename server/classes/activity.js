a71e8659799ad6202056333a6f27d89c84d2a110
Garry Hall <garrymhall@gmail.com>
2021-08-06 12:33:56 +0100

    added populate.js file and begin to refactor the database seed

--- server/classes/activity.js
+++ server/classes/activity.js
@@ -1,9 +1,11 @@
 class Activity {
-    constructor(title, charity, description, duration, location, noOfPoints) {
+    constructor(title, charity, description, duration, location, points) {
         this.title = title;
         this.charity = charity;
         this.duration = duration;
         this.location = location;
-        this.noOfPoints = noOfPoints;
+        this.points = points;
     }
 }
+
+module.exports = Activity;
