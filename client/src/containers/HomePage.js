d403117fde7b5065f64bd67af0a09baba2d68281
sarahgraham1982 <79756187+sarahgraham1982@users.noreply.github.com>
2021-08-06 14:16:18 +0100

    sorted some errors

--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -1,13 +1,16 @@
 import React from "react";
+import '../css/HomePage.css';
 
-const HomePage = () => (
-  <div>
-    <h4>Home</h4>
-  </div>
-
-  <div className="welcome-image">
-    <img src="/welcome.png" alt="welcome" width="500" height="500" />
-  </div>
-);
+const HomePage = () => {
+  return (
+    <div>
+      <h4>Home</h4>
+    </div>
+    <div className="right">
+      <div className="welcome-image">
+        <img src="/welcome.png" alt="welcome" width="500" height="500" />
+      </div>
+  );
+}
 
 export default HomePage;
